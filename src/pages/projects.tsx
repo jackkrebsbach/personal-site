b84f146f1756887cb84b882c282db0790e1f6d02
Jack Krebsbach <krebsbachjackson@gmail.com>
2021-09-22 22:51:04 -0400

    hamburger icon

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/reusables/layout/Layout";
 export default function projects() {
   return (
     <Layout>
-      <h1> my projects</h1>
+      <h1>projects</h1>
     </Layout>
   );
 }
