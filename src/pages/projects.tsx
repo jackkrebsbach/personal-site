028164e1bcf09c5c7b39369c1110e713d97225e9
Jack Krebsbach <krebsbachjackson@gmail.com>
2021-09-26 11:08:13 -0400

    ref glitch fix in projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,9 +2,13 @@ import Layout from "../components/reusables/layout/Layout";
 import styles from "../styles/Projects.module.scss";
 
 export default function projects() {
-  function handleClick(e: any) {
+  function handleSoftwareClick(e: any) {
     e.preventDefault();
-    window.open(`/projects/${e.target.id}.pdf`, "_blank");
+    window.open("/projects/software.pdf", "_blank");
+  }
+  function handleDataClick(e: any) {
+    e.preventDefault();
+    window.open("/projects/data-science.pdf", "_blank");
   }
   return (
     <Layout>
@@ -13,13 +17,13 @@ export default function projects() {
           <h2> Web Development </h2>
         </div>
 
-        <button id="software" onClick={handleClick}>
+        <button id="software" onClick={handleSoftwareClick}>
           <span>ENTER</span>
         </button>
         <div className={styles.header__wrapper}>
           <h2> Data Science </h2>
         </div>
-        <button id="data-science" onClick={handleClick}>
+        <button id="data-science" onClick={handleDataClick}>
           <span>ENTER</span>
         </button>
       </div>
