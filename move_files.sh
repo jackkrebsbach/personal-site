d5b88db6406f329ea00cf1d56a6271fd57e60150
Jack Krebsbach <krebsbachjackson@gmail.com>
2024-01-13 17:46:06 -0500

    add pdf version

--- move_files.sh
+++ move_files.sh
@@ -1,5 +1,6 @@
 
 mkdir ./public/index_files/
 cp ~/ml/portfolio/index.html ./public/
+cp ~/ml/portfolio/index.pdf ./public/
 cp -rf ~/ml/portfolio/index_files/* ./public/index_files/
 cp ~/Documents/misc/internships/resume/resume.pdf ./public/
