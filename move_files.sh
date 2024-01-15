4abb09b078d9be80fb2e3a4ab1c714a735966d51
Jack Krebsbach <krebsbachjackson@gmail.com>
2024-01-14 21:37:03 -0500

    add cv

--- move_files.sh
+++ move_files.sh
@@ -4,3 +4,4 @@ cp ~/ml/portfolio/index.html ./public/
 cp ~/ml/portfolio/index.pdf ./public/
 cp -rf ~/ml/portfolio/index_files/* ./public/index_files/
 cp ~/Documents/misc/internships/resume/resume.pdf ./public/
+cp ~/Documents/misc/internships/cv/cv.pdf ./public/
--- src/components/reusables/footer/Footer.tsx
+++ src/components/reusables/footer/Footer.tsx
@@ -1,12 +1,22 @@
 /* eslint-disable react/jsx-no-target-blank */
 import { FC, useState } from "react";
 import styles from "./Footer.module.scss";
+import Link from "next/link";
 
 export const Footer: FC = () => {
   const [d, _] = useState<number>(new Date().getFullYear());
   return (
     <footer className={styles.container}>
       <span> © {d} JACK KREBSBACH</span>
+      <span>
+        Looking for My CV? Click{" "}
+        <span className={styles.underline}>
+          <Link href="/cv.pdf" target="_blank">
+            {" "}
+            Here
+          </Link>
+        </span>
+      </span>
       <div className={styles.links}>
         <a href="https://github.com/jackkrebsbach" target="_blank">
           <i className="fab fa-github" />
