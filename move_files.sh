2621fc3f5ac1aa1e10d754ef5da292c98bf53520
Jack Krebsbach <krebsbachjackson@gmail.com>
2024-03-12 10:59:13 -0400

    remove quarto files & update cv

--- move_files.sh
+++ move_files.sh
@@ -1,7 +1,7 @@
 
 mkdir ./public/index_files/
-cp ~/ml/portfolio/index.html ./public/
+#cp ~/ml/portfolio/index.html ./public/
 cp ~/ml/portfolio/index.pdf ./public/
-cp -rf ~/ml/portfolio/index_files/* ./public/index_files/
+#cp -rf ~/ml/portfolio/index_files/* ./public/index_files/
 cp ~/Documents/misc/internships/resume/resume.pdf ./public/
 cp ~/Documents/misc/internships/cv/cv.pdf ./public/
